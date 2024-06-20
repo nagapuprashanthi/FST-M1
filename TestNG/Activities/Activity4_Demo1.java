761b5993b1f46496d5d439ffdf9413ff9bd11de6
nagapuprashanthi <NAGAPURI.PRASHANTHI@ibm.com>
2024-06-20 10:46:20 +0530

    Update Activity4_Demo1.java

--- TestNG/Activities/Activity4_Demo1.java
+++ TestNG/Activities/Activity4_Demo1.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class Activity4_Demo1 {
         @Test
         public void firstTestCase() {
-            System.out.println("I'm in first test case from demoOne Class");
+            System.out.println("test case from demoOne Class");
         }
 
         @Test
         public void secondTestCase() {
-            System.out.println("I'm in second test case from demoOne Class");
+            System.out.println("second test case from demoOne Class");
         }
     }
